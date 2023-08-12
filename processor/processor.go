5fb2c1c5c25a196837dcc539a8827ce0d552a773
DavidEsdrs <carpina1983@gmail.com>
2023-08-12 08:52:13 -0300

    refactor: Update flip function to match the new way that images are saved

--- processor/processor.go
+++ processor/processor.go
@@ -26,7 +26,7 @@ type ImageProcessor struct {
 	processes []Process
 }
 
-func flipX(pImg *[][]color.Color) {
+func flipY(pImg *[][]color.Color) {
 	img := *pImg
 	rows := len(img)
 	cols := len(img[0])
@@ -38,11 +38,11 @@ func flipX(pImg *[][]color.Color) {
 	}
 }
 
-func (ip *ImageProcessor) FlipX() {
-	ip.processes = append(ip.processes, flipX)
+func (ip *ImageProcessor) FlipY() {
+	ip.processes = append(ip.processes, flipY)
 }
 
-func flipY(pImg *[][]color.Color) {
+func flipX(pImg *[][]color.Color) {
 	img := *pImg
 	rows := len(img)
 	cols := len(img[0])
@@ -54,12 +54,36 @@ func flipY(pImg *[][]color.Color) {
 	}
 }
 
-func (ip *ImageProcessor) FlipY() {
-	ip.processes = append(ip.processes, flipY)
+func (ip *ImageProcessor) FlipX() {
+	ip.processes = append(ip.processes, flipX)
+}
+
+func turnLeft(pImg *[][]color.Color) {
+	img := *pImg
+
+	originalRows := len(img)
+	originalCols := len(img[0])
+
+	rows := len(img[0])
+	cols := len(img)
+
+	res := make([][]color.Color, rows)
+
+	for i := range res {
+		res[i] = make([]color.Color, cols)
+	}
+
+	for i := 0; i < originalRows; i++ {
+		for j := 0; j < originalCols; j++ {
+			res[rows-j-1][i] = img[i][j]
+		}
+	}
+
+	*pImg = res
 }
 
 func (ip *ImageProcessor) TurnLeft() {
-	log.Fatal("Turn left not implemented")
+	ip.processes = append(ip.processes, turnLeft)
 }
 
 func (ip *ImageProcessor) TurnRight() {
