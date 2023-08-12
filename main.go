7d0dd52791c7e4879710b4329d7fdf1ff7d08599
DavidEsdrs <carpina1983@gmail.com>
2023-08-12 08:48:54 -0300

    refactor: Simplify convert logic

--- main.go
+++ main.go
@@ -33,30 +33,24 @@ type Config struct {
 // Convert the image into a tensor to further manipulation
 func convertIntoTensor(img image.Image) [][]color.Color {
 	size := img.Bounds().Size()
-	var pixels [][]color.Color
-	for i := 0; i < size.X; i++ {
-		var y []color.Color
-		for j := 0; j < size.Y; j++ {
-			y = append(y, img.At(i, j))
+	pixels := make([][]color.Color, size.Y)
+
+	for y := 0; y < size.Y; y++ {
+		pixels[y] = make([]color.Color, size.X)
+		for x := 0; x < size.X; x++ {
+			pixels[y][x] = img.At(x, y)
 		}
-		pixels = append(pixels, y)
 	}
+
 	return pixels
 }
 
 func convertIntoImage(pixels [][]color.Color) image.Image {
-	rect := image.Rect(0, 0, len(pixels), len(pixels[0]))
+	rect := image.Rect(0, 0, len(pixels[0]), len(pixels))
 	nImg := image.NewRGBA(rect)
-	for x := 0; x < len(pixels); x++ {
-		for y := 0; y < len(pixels[0]); y++ {
-			q := pixels[x]
-			if q == nil {
-				continue
-			}
-			p := pixels[x][y]
-			if p == nil {
-				continue
-			}
+	for y := 0; y < len(pixels); y++ {
+		for x := 0; x < len(pixels[0]); x++ {
+			p := pixels[y][x]
 			original, ok := color.RGBAModel.Convert(p).(color.RGBA)
 			if ok {
 				nImg.Set(x, y, original)
