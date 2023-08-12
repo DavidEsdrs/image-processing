70a70bc81ace701928563a9b10a711154acdfe6e
DavidEsdrs <carpina1983@gmail.com>
2023-08-12 09:46:56 -0300

    fix(convertIntoImage): Check if pixel is nil before trying to convert

--- main.go
+++ main.go
@@ -52,7 +52,14 @@ func convertIntoImage(pixels [][]color.Color) image.Image {
 	nImg := image.NewRGBA(rect)
 	for y := 0; y < len(pixels); y++ {
 		for x := 0; x < len(pixels[0]); x++ {
+			q := pixels[y]
+			if q == nil {
+				continue
+			}
 			p := pixels[y][x]
+			if p == nil {
+				continue
+			}
 			original, ok := color.RGBAModel.Convert(p).(color.RGBA)
 			if ok {
 				nImg.Set(x, y, original)
