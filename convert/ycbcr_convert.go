eacbe3f9ca6781138ceae07191f04113be7bd89d
DavidEsdrs <carpina1983@gmail.com>
2023-08-14 13:06:38 -0300

    fix: Get sumbsampling ratio from the context and calculate Y and C offset independently

--- convert/ycbcr_convert.go
+++ convert/ycbcr_convert.go
@@ -7,20 +7,23 @@ import (
 	_ "image/png"
 )
 
-type YcbcrStrategy struct{}
+type YcbcrStrategy struct {
+	subsamplingRatio image.YCbCrSubsampleRatio
+}
 
 func (pstr *YcbcrStrategy) Convert(pixels [][]color.Color) image.Image {
 	rect := image.Rect(0, 0, len(pixels[0]), len(pixels))
-	nImg := image.NewYCbCr(rect, image.YCbCrSubsampleRatio444)
+	nImg := image.NewYCbCr(rect, pstr.subsamplingRatio)
 	for y := 0; y < len(pixels); y++ {
 		for x := 0; x < len(pixels[0]); x++ {
 			p := pixels[y][x]
 			ycbcrColor, ok := p.(color.YCbCr)
 			if ok {
-				offset := nImg.YOffset(x, y)
-				nImg.Y[offset] = ycbcrColor.Y
-				nImg.Cb[offset] = ycbcrColor.Cb
-				nImg.Cr[offset] = ycbcrColor.Cr
+				yoffset := nImg.YOffset(x, y)
+				coffset := nImg.COffset(x, y)
+				nImg.Y[yoffset] = ycbcrColor.Y
+				nImg.Cb[coffset] = ycbcrColor.Cb
+				nImg.Cr[coffset] = ycbcrColor.Cr
 			}
 		}
 	}
