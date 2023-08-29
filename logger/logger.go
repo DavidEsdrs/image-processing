dd77c9a546b6b052f9f867dbadcc1ea5003dfe7c
DavidEsdrs <carpina1983@gmail.com>
2023-08-29 03:00:18 -0300

    feat: Create some utilities logging functions

--- logger/logger.go
+++ logger/logger.go
@@ -1,20 +1,52 @@
 package logger
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 type Logger struct {
 	Warn    bool
 	Process bool
+	logger  *log.Logger
 }
 
-func (l *Logger) LogWarn(format, msg string) {
-	if l.Warn {
-		fmt.Printf(format, msg)
+func NewLogger(verbose bool) Logger {
+	flags := log.Ldate | log.Ltime // log date and time
+	logger := log.New(os.Stdout, "", flags)
+	return Logger{
+		Warn:    verbose,
+		Process: verbose,
+		logger:  logger,
 	}
 }
 
-func (l *Logger) LogProcess(format, msg string) {
+func (l *Logger) Log(msg string) {
 	if l.Process {
-		fmt.Printf(format, msg)
+		l.logger.Println(msg)
+	}
+}
+
+func (l *Logger) LogWarnf(format string, msg ...any) {
+	if l.Warn {
+		l.logger.Printf(format, msg...)
+	}
+}
+
+func (l *Logger) LogProcessf(format string, msg ...any) {
+	if l.Process {
+		l.logger.Printf(format, msg...)
+	}
+}
+
+func (l *Logger) LogWarn(msg string) {
+	if l.Warn {
+		l.logger.Println(msg)
+	}
+}
+
+func (l *Logger) LogProcess(msg string) {
+	if l.Process {
+		l.logger.Println(msg)
 	}
 }
