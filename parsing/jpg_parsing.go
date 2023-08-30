7fd93d07dd8c74b4ed034a87baa1fe89a7aa29ff
DavidEsdrs <carpina1983@gmail.com>
2023-08-30 20:24:52 -0300

    fix: Check if quality is greater than 0 instead of checking if quality is different to 0

--- parsing/jpg_parsing.go
+++ parsing/jpg_parsing.go
@@ -17,7 +17,7 @@ func (jps *JpgParsingStrategy) Save(img image.Image, outputPath string) error {
 	}
 	defer fg.Close()
 	quality := configs.GetConfig().Quality
-	if quality != 0 {
+	if quality > 0 {
 		err = jpeg.Encode(fg, img, &jpeg.Options{
 			Quality: quality,
 		})
