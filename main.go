e5b9a0a615c1ef8fe35d8476a17cc5ce101a7b63
DavidEsdrs <carpina1983@gmail.com>
2023-08-31 05:19:54 -0300

    refactor: Move main() to the top of the file

--- main.go
+++ main.go
@@ -19,72 +19,12 @@ import (
 	"github.com/DavidEsdrs/image-processing/utils"
 )
 
-func processImage(img image.Image, outputPath string, proc processor.Processor, logger *logger.Logger) error {
-	logger.LogProcess("Converting image into tensor")
-	tensor := utils.ConvertIntoTensor(img)
-
-	iep := proc.Execute(&tensor)
-
-	context := convert.NewConversionContext(logger)
-
-	conversor, err := context.GetConversor(img, proc.GetColorModel())
-
-	if err != nil {
-		return err
-	}
-
-	cImg := conversor.Convert(iep)
-
-	pc := parsing.NewParsingContext(logger)
-
-	config, err := pc.GetConfig()
-
-	if err != nil {
-		return err
-	}
-
-	logger.LogProcessf("Saving image as %v", outputPath)
-	config.Save(cImg, outputPath)
-	return nil
-}
-
-// set cli flags
-//
-// TODO: Avoid using pointer to primitive args
-func SetFlags(config *configs.Config, verbose *bool, help *bool) {
-	flag.BoolVar(help, "h", false, "Print tool usage")
-	flag.BoolVar(verbose, "v", false, "Verbose")
-
-	flag.StringVar(&config.Input, "i", "", "Input file")
-	flag.StringVar(&config.Output, "o", "", "Output file")
-
-	// filters
-	flag.BoolVar(&config.FlipY, "fy", false, "Flip y axis filter")
-	flag.BoolVar(&config.FlipX, "fx", false, "Flip x axis filter")
-	flag.BoolVar(&config.Transpose, "t", false, "Apply transpose process (rotate 270 degrees and flip Y axis)")
-	flag.BoolVar(&config.TurnLeft, "tl", false, "Rotate 90 degrees counterclockwise")
-	flag.BoolVar(&config.TurnRight, "tr", false, "Rotate 90 degrees clockwise")
-	flag.BoolVar(&config.Grayscale, "gs", false, "Apply grayscale filter")
-	flag.Float64Var(&config.NearestNeighbor, "nn", 1.0, "Apply nearest neighbor resize algorithm")
-	flag.StringVar(&config.Crop, "c", "", "Crop image at given coordinates. Ex.: \"-c 0,1000,0,200\", xstart,xend,ystart,yend or \"-c 1000,200\", xend,yend (x and y start default to 0)")
-	flag.IntVar(&config.Ssr, "ssr", 0, "Subsample ratio for images YCbCr. 444 = 4:4:4, 422 = 4:2:2, 420 = 4:2:0, 440 = 4:4:0, 411 = 4:1:1, 410 = 4:1:0")
-	flag.IntVar(&config.Quality, "q", 0, "Quality of the JPEG image. 1-100")
-
-	// overlay
-	flag.StringVar(&config.Overlay, "ov", "", "Image to overlay onto the input image")
-	flag.IntVar(&config.DistTop, "dt", 0, "Distance to the top")
-	flag.IntVar(&config.DistRight, "dr", 0, "Distance to the right")
-	flag.IntVar(&config.DistBottom, "db", 0, "Distance to the bottom")
-	flag.IntVar(&config.DistLeft, "dl", 0, "Distance to the left")
-	flag.BoolVar(&config.Fill, "fill", false, "Should the overlay fill in")
-}
-
 func main() {
 	var config *configs.Config = configs.GetConfig()
 	var verbose bool
 	var help bool
 
-	SetFlags(config, &verbose, &help)
+	setFlags(config, &verbose, &help)
 
 	flag.Parse()
 
@@ -127,4 +67,64 @@ func main() {
 	duration := time.Since(start)
 
 	logger.LogProcessf("completed: image %v processed - %v\n", file, duration.String())
+}
+
+func processImage(img image.Image, outputPath string, proc processor.Processor, logger *logger.Logger) error {
+	logger.LogProcess("Converting image into tensor")
+	tensor := utils.ConvertIntoTensor(img)
+
+	iep := proc.Execute(&tensor)
+
+	context := convert.NewConversionContext(logger)
+
+	conversor, err := context.GetConversor(img, proc.GetColorModel())
+
+	if err != nil {
+		return err
+	}
+
+	cImg := conversor.Convert(iep)
+
+	pc := parsing.NewParsingContext(logger)
+
+	config, err := pc.GetConfig()
+
+	if err != nil {
+		return err
+	}
+
+	logger.LogProcessf("Saving image as %v", outputPath)
+	config.Save(cImg, outputPath)
+	return nil
+}
+
+// set cli flags
+//
+// TODO: Avoid using pointer to primitive args
+func setFlags(config *configs.Config, verbose *bool, help *bool) {
+	flag.BoolVar(help, "h", false, "Print tool usage")
+	flag.BoolVar(verbose, "v", false, "Verbose")
+
+	flag.StringVar(&config.Input, "i", "", "Input file")
+	flag.StringVar(&config.Output, "o", "", "Output file")
+
+	// filters
+	flag.BoolVar(&config.FlipY, "fy", false, "Flip y axis filter")
+	flag.BoolVar(&config.FlipX, "fx", false, "Flip x axis filter")
+	flag.BoolVar(&config.Transpose, "t", false, "Apply transpose process (rotate 270 degrees and flip Y axis)")
+	flag.BoolVar(&config.TurnLeft, "tl", false, "Rotate 90 degrees counterclockwise")
+	flag.BoolVar(&config.TurnRight, "tr", false, "Rotate 90 degrees clockwise")
+	flag.BoolVar(&config.Grayscale, "gs", false, "Apply grayscale filter")
+	flag.Float64Var(&config.NearestNeighbor, "nn", 1.0, "Apply nearest neighbor resize algorithm")
+	flag.StringVar(&config.Crop, "c", "", "Crop image at given coordinates. Ex.: \"-c 0,1000,0,200\", xstart,xend,ystart,yend or \"-c 1000,200\", xend,yend (x and y start default to 0)")
+	flag.IntVar(&config.Ssr, "ssr", 0, "Subsample ratio for images YCbCr. 444 = 4:4:4, 422 = 4:2:2, 420 = 4:2:0, 440 = 4:4:0, 411 = 4:1:1, 410 = 4:1:0")
+	flag.IntVar(&config.Quality, "q", 0, "Quality of the JPEG image. 1-100")
+
+	// overlay
+	flag.StringVar(&config.Overlay, "ov", "", "Image to overlay onto the input image")
+	flag.IntVar(&config.DistTop, "dt", 0, "Distance to the top")
+	flag.IntVar(&config.DistRight, "dr", 0, "Distance to the right")
+	flag.IntVar(&config.DistBottom, "db", 0, "Distance to the bottom")
+	flag.IntVar(&config.DistLeft, "dl", 0, "Distance to the left")
+	flag.BoolVar(&config.Fill, "fill", false, "Should the overlay fill in")
 }
