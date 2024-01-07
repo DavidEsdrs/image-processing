4c9247931fd63611a3b117bb698d8659b7ed5ae0
DavidEsdrs <carpina1983@gmail.com>
2024-01-07 03:50:34 -0300

    docs: improve helper documentation

--- logger/logger.go
+++ logger/logger.go
@@ -20,13 +20,15 @@ Crop Options:
 										Format: "-c xstart,xend,ystart,yend" or "-c xend,yend"
 
 Image Processing Options:
-	-nn float        Apply nearest neighbor resize algorithm (default 1)
+	-nn bool         Indicate nearest neighbor resize algorithm must be used
+	-width int       Output width
+	-height int      Output height
 	-q int           Quality of the JPEG image (1-100)
 	-ssr int         Subsample ratio for images YCbCr
 	-fx              Flip x-axis filter
 	-fy              Flip y-axis filter
 	-gs              Apply grayscale filter
-	-pb		           Add padding to the background image for the overlay to ensure full visibility. 
+	-pb              Add padding to the background image for the overlay to ensure full visibility. 
 	-t               Apply transpose process (rotate 270 degrees and flip Y axis)
 	-tl              Rotate 90 degrees counterclockwise
 	-tr              Rotate 90 degrees clockwise
