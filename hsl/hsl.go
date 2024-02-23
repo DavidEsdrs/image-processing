200bb684c034ce3703fa8e30b22e891a3ee551fb
DavidEsdrs <carpina1983@gmail.com>
2024-02-22 22:34:28 -0300

    fix: fix rgb to hsl conversion

--- hsl/hsl.go
+++ hsl/hsl.go
@@ -2,14 +2,15 @@ package hsl
 
 import (
 	"image/color"
+	"math"
 
 	"github.com/DavidEsdrs/image-processing/utils"
 )
 
 type HSL struct {
-	H float32
-	S float32
-	L float32
+	H float64
+	S float64
+	L float64
 }
 
 func (hsl HSL) RGBA() (r, g, b, a uint32) {
@@ -22,11 +23,11 @@ func (hsl HSL) RGBA() (r, g, b, a uint32) {
 	}
 
 	var (
-		temp1 float32
-		temp2 float32
-		tempR float32
-		tempG float32
-		tempB float32
+		temp1 float64
+		temp2 float64
+		tempR float64
+		tempG float64
+		tempB float64
 	)
 
 	if hsl.L < 0.5 {
@@ -37,7 +38,7 @@ func (hsl HSL) RGBA() (r, g, b, a uint32) {
 
 	temp2 = 2*hsl.L - temp1
 
-	fixedH := float32(hsl.H) / 360
+	fixedH := float64(hsl.H) / 360
 
 	tempR = fixedH + 1.0/3
 
@@ -62,53 +63,50 @@ func (hsl HSL) RGBA() (r, g, b, a uint32) {
 
 func ColorToHsl(color color.Color) HSL {
 	var (
-		H float32
-		S float32
-		L float32
+		H float64
+		S float64
+		L float64
 	)
 
 	R, G, B, _ := color.RGBA()
 
-	r := float32(R) / 0xffff
-	g := float32(G) / 0xffff
-	b := float32(B) / 0xffff
+	r := float64(R) / 0xffff
+	g := float64(G) / 0xffff
+	b := float64(B) / 0xffff
 
 	min := utils.Min(r, g, b)
 	max := utils.Max(r, g, b)
 	c := max - min
 
-	L = float32(min+max) / 2
+	s := float64(min+max) / 2.0
+	H = s
+	S = s
+	L = s
 
-	if min == max {
-		H = 0
-		S = 0
-	} else {
-		if L <= 0.5 {
-			S = float32(c) / float32(max+min)
-		} else {
-			S = float32(c) / float32(2-c)
-		}
-
+	if c > 0 {
 		switch max {
 		case r:
 			H = (g - b) / c
+			if H < 0 {
+				H += 6
+			}
 		case g:
-			H = 2 + (b-r)/c
+			H = 2.0 + (b-r)/c
 		case b:
-			H = 4 + (r-g)/c
+			H = 4.0 + (r-g)/c
 		}
 
 		H *= 60
+	}
 
-		if H < 0 {
-			H += 360
-		}
+	if c != 0 {
+		S = c / (1 - math.Abs(2*L-1))
 	}
 
 	return HSL{H, S, L}
 }
 
-func hueToRGB(channel, temp1, temp2 float32) float32 {
+func hueToRGB(channel, temp1, temp2 float64) float64 {
 	if 6*channel < 1 {
 		return temp2 + (temp1-temp2)*6*channel
 	} else if 2*channel < 1 {
